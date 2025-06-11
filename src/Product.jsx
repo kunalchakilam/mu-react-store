ef69fdd0e78e2b7928a0a98b5698e3ff8150d6ef
kunalchakilam <ikunalchakilam@gmail.com>
2025-06-11 10:26:06 +0530

    Updated

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 export default function Product() {
+    const fetchProducts = () => {
+        alert("Hello World");
+    };
+    useEffect(()=>{
+        fetchProducts
+    },[])
   return (
     <div>Product</div>
   )
