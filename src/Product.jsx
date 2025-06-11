d8184966142b79608df43c3f13a893e4a858a940
kunalchakilam <ikunalchakilam@gmail.com>
2025-06-11 10:59:44 +0530

    Updated

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
+import axios from 'axios';
 
 export default function Product() {
-    const [ count, setCount ] = useState(0);
-    const fetchProducts = () => {
-        alert("Hello World");
+    const [products, setProducts] = useState([]);
+    const fetchProducts = async () => {
+        const url = "https://gcet-node-app.vercel.app/products/all"
+        const res = await axios.get(url);
+        setProducts(res.data);
     };
     useEffect(()=>{
-        fetchProducts();
-        return () =>{
-            alert("Unmounted");
-        };
+        fetchProducts();   
     },[count])
   return (
     <div>
-        <p>{count}</p>
-        <button onClick={()=>setCount(count+1)}>Update Count</button>
+        {products.map(product=>(
+            <li>{product.name}</li>
+        ))}
     </div>
   )
 }
